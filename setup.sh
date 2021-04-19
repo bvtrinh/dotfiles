2c2d6792c74fc6a4181443e75ded0e5952419f24
Tyler Trinh <tylervtrinh@gmail.com>
2021-04-19 03:59:57 -0400

    refactor: fix software install commands

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 # Setup constants
+HOME=/home/ubuntu;
 INSTALL_DIR=~/tmp/install;
-VSCODE_FILE=vscode_extensions.txt;
-VIM_PKG_DIR=~/.vim/bundle/;
-PACKAGES_FILE=package_list.txt;
+VSCODE_FILE=~/.dotfiles/vscode_extensions.txt;
+VIM_PKG_DIR=~/.vim/bundle;
+PACKAGES_FILE=packages_list.txt;
 ALL_FLAG=0;
 
 while getopts ":ha" opt; do
@@ -19,27 +20,22 @@ while getopts ":ha" opt; do
   esac
 done
 
-main;
-
-error() { clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;}
-
 node_install() {
     curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -;
     apt install -y nodejs;
 }
 
 discord_install() {
-    wget -O discord.deb https://discordapp.com/api/download?platform=linux&format=deb;
-    dpkg -i discord.deb;
+    snap install discord;
 }
 
 zoom_install() {
     wget -O zoom.deb https://zoom.us/client/latest/zoom_amd64.deb;
-    dpkg -i zoom.deb;
+    apt install ./zoom.deb;
 }
 
 ngrok_install() {
-    wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz;
+    wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
     unzip ngrok.zip;
     mv ngrok /usr/local/bin/;
 }
@@ -60,7 +56,7 @@ betterlockscreen_install() {
 
 google_chrome_install() {
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
-    sudo dpkg -i google-chrome-stable_current_amd64.deb;
+    apt install -y ./google-chrome-stable_current_amd64.deb;
 }
 
 postman_install() {
@@ -78,29 +74,27 @@ virtualenv_install() {
 }
 
 vscode_install() {
-    wget -O vscode.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64;
-    apt install ./vscode.deb;
-    # FIXME: Might need to do a loop here
-    xargs code --install-extension < $VSCODE_FILE;
+    snap install --classic code;
+    cat $VSCODE_FILE | xargs -L1 code --user-data-dir ubuntu --install-extension < $VSCODE_FILE;
 }
 
 vim_setup() {
-    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    mkdir -p ~/.vim/autoload ~/.vim/bundle;
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
 
     # Install plugins
-    git clone https://github.com/vim-airline/vim-airline $VIM_PKG_DIR; 
-    git clone git://github.com/altercation/vim-colors-solarized.git $VIM_PKG_DIR;
+    git clone https://github.com/vim-airline/vim-airline $VIM_PKG_DIR/vim-airline; 
+    git clone git://github.com/altercation/vim-colors-solarized.git $VIM_PKG_DIR/vim-colors-solarized;
 }
 
-# Missing: light, vscode setup
+# Missing: light, deluge, postman, virtualenv, betterlockscreen
 software_install() {
-    node_install || error "Unable to install nodejs.";
-    discord_install || error "Unable to install discord.";
-    zoom_install || error "Unable to install zoom.";
-    ngrok_install || error "Unable to install ngrok.";
-    google_chrome_install || error "Unable to install google-chrome.";
-    vscode_install || error "Unable to install vscode";
+    node_install;
+    discord_install;
+    zoom_install;
+    ngrok_install;
+    google_chrome_install;
+    vscode_install;
 }
 
 symlink_setup() {
@@ -113,26 +107,30 @@ symlink_setup() {
 update_shell() {
     # oh-my-zsh
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-    chsh -s /bin/zsh "$name" >/dev/null 2>&1;
+    chsh -s /bin/zsh ubuntu >/dev/null 2>&1;
 }
 
 
 main() {
     # Update, upgrade and install packages specified in $PACKAGES_FILE
-    apt update && apt upgrade;
-    xargs apt install < $PACKAGES_FILE;
-    vim_setup || error "Unable to setup vim plugins."
+    #apt -y update && apt -y upgrade;
+    #xargs apt -y install < $PACKAGES_FILE;
+    #vim_setup;
 
-    mkdir -p $INSTALL_DIR && cd $INSTALL_DIR;
+    #mkdir -p $INSTALL_DIR;
+    cd $INSTALL_DIR;
 
     if [[ "$ALL_FLAG" == 1 ]]; then
         software_install;
     fi
 
-    symlink_setup || error "Unable to setup symlinks.";
+    #symlink_setup || error "Unable to setup symlinks.";
     update_shell || error "Unable to update shell";
 
     echo "No errors! Hurray!";
+    cd ..;
     exit 0;
 }
+
+main;
 
